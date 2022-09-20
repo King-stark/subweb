37870ad8aa34862c2b31e72324ff7e84f91b8c02
king stark <63186186+King-stark@users.noreply.github.com>
2022-09-20 12:36:54 +0800

    Update config.js

--- conf/config.js
+++ conf/config.js
@@ -3,20 +3,20 @@ window.config = {
   apiUrl: 'https://api.leaf.tk',
   shortUrl: 'https://leaf.tk',
   menuItem: [
-    {
-      title: '首页',
-      link: '/',
-      target: '',
-    },
+//     {
+//       title: '首页',
+//       link: '/',
+//       target: '',
+//     },
     {
       title: '短链接',
       link: 'https://leaf.tk',
       target: '_blank',
     },
-    {
-      title: 'GitHub',
-      link: 'https://github.com/king-stark/subweb',
-      target: '_blank',
-    },
+//     {
+//       title: 'GitHub',
+//       link: 'https://github.com/king-stark/subweb',
+//       target: '_blank',
+//     },
   ],
 };
